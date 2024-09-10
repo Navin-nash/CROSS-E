98cbbdb1dcc2c865f590349c15627203ac028253
Navin Raj <101266613+Navin-nash@users.noreply.github.com>
2024-09-10 22:00:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,10 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({origin: 'https://cross-e.vercel.app/', 
-  methods: 'GET,POST,PUT,DELETE',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
